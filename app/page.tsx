2b81a5d95374323162516ceb2de980ca67ba0590
Nick <darkforceyoda@gmail.com>
2023-07-23 22:17:11 -0500

    installed valtio, playerCount passed via state management

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,50 @@
 "use client";
-import { useState } from "react";
-import Player from "./components/player/Player";
+import React from "react";
+import { useSnapshot } from "valtio";
+import { state } from "./va;tio/state";
 
-export default function Home() {
-  const [playerCount, setPlayerCount] = useState(3);
+type pageProps = {};
+
+const page: React.FC<pageProps> = () => {
+  const snap = useSnapshot(state);
   return (
-    <main
-      className={
-        playerCount > 2
-          ? `grid grid-cols-2 w-screen h-screen`
-          : `grid grid-rows-2 w-screen h-screen`
-      }
-    >
-      {[...Array(playerCount)].map((player) => (
-        <Player />
-      ))}
-    </main>
+    <div className="bg-blue-400 h-screen w-screen flex flex-col items-center justify-center gap-4">
+      <h2 className="text-3xl font-bold">Player Count {snap.playerCount}</h2>
+      <div className="flex justify-center items-center gap-8">
+        <button
+          onClick={() => {
+            state.playerCount = 1;
+          }}
+          className="bg-gray-200 p-8 rounded-lg shadow-md hover:bg-gray-300"
+        >
+          1
+        </button>
+        <button
+          onClick={() => {
+            state.playerCount = 2;
+          }}
+          className="bg-gray-200 p-8 rounded-lg shadow-md hover:bg-gray-300"
+        >
+          2
+        </button>
+        <button
+          onClick={() => {
+            state.playerCount = 3;
+          }}
+          className="bg-gray-200 p-8 rounded-lg shadow-md hover:bg-gray-300"
+        >
+          3
+        </button>
+        <button
+          onClick={() => {
+            state.playerCount = 4;
+          }}
+          className="bg-gray-200 p-8 rounded-lg shadow-md hover:bg-gray-300"
+        >
+          4
+        </button>
+      </div>
+    </div>
   );
-}
+};
+export default page;
